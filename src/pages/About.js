53cc24d0a4ad5945672754da6f15e64d3dd69782
Gabriel Fairbairn A00888969 / NerdSlayer0 <gabrielfair@hotmail.com>
2023-06-07 10:46:37 -0700

    added: json-server, useEffect, useState, mapped data, HomePage, custom setYourName function

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import { useState, useEffect } from "react";
 
-export default function about() {
+const GetData = () => {
+  const [workExperience, setWorkExperience] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8000/work_experience")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setWorkExperience(data);
+      });
+  }, []);
+  
   return (
-    <div>
-        TEST
-    </div>
-  )
-}
+  <div>
+    { workExperience.map((job) => (
+        <div key={job.company}>{job.company}</div>
+    ))
+    } </div>
+  );
+};
+
+const About = () => {
+  return <div>TEST</div>;
+};
+
+export { GetData, About };
--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,45 +1,33 @@
 import { useState } from "react";
 // import getData from "./api/fetch";
-// import Flex from "@chakra-ui/react"
 
+// TODO: Change document.body to a guest list array as a useState, add much more chakra-ui
 const Welcome = () => {
   const [yourName, setYourName] = useState('');
   
+  const printName = (param) => {
+    const box = document.createElement("box");
+    box.innerText = "Gabe has now met: " + param + "\n";
+    document.body.append(box);
+  }
+  
   return (
     <>
-      <form>
+
         <label>Welcome to Geb's page, please introduce yourself <br /></label>
         <br />
         <input 
         type="text" 
         value={ yourName }
         onChange={(e) => setYourName(e.target.value)}/>
-      </form>
+        <br />
+        <button onClick={() => printName(yourName)}>Sign the Guest List</button>
+        <br /><br />
       
       <div>Hi Geb, my name is { yourName }</div>
       
     </>
   );
 };
-
-// const About = () => {
-//     const [data, setData] = useState(null);
-//     useEffect(() => {
-//         getData()
-//     });
-
-//   return (
-//     <div>
-//       <p>
-//         My nem is Geb!
-//         <code>About me:</code>
-
-//         <Flex>
-
-//         </Flex>
-//       </p>
-//     </div>
-//   );
-// }
 
 export default Welcome;
